1ed7a7c91a23009fd2dadf6927741627bc16741a
clja1970 <pepecl2020@gmail.com>
2021-02-16 12:12:40 +0100

    Programa que indica el número mayor

---
+++ src/es/studium/ejrcicio1_tema_5/Ejercicio1Tema5.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+# 
+#  format.py
+#
+'''
+Created on 16 feb. 2021
+
+@author: clja1
+'''
+'''Definir una funci�n max() que tome como argumento dos n�meros y
+devuelva el mayor de ellos. (Es cierto que Python tiene una funci�n max()
+incorporada, pero hacerla nosotros mismos es un muy buen ejercicio.'''
+print ("El número mayor")
+x=int(input ("introduce un número entero: "))
+y=int(input ("introduce otro número entero: "))
+def fun (x,y):
+    if x>y:
+        return (x)
+    if y>x:
+        return (y)
+        
+numeros=fun(x, y)
+print (numeros)
+   
+    
+    
